fd39076827c32b97bdbf34d61bbf3d3311b09e80
tanuja <tanukam59@gmail.com>
2018-10-11 19:59:43 +0530

    my project setup completed sucessfully

---
+++ shoppingbackend/src/main/java/net/tanuja/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.tanuja.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
